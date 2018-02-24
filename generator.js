3ae538afa4df1204bb5df02dbaa57e0feaffe87a
Osiris <34219959+OsirisRex@users.noreply.github.com>
2018-02-23 21:00:24 -0800

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,33 @@
+var test = [
+  'Apple',
+  'Banana',
+  'Cabbage',
+  'Dead Fish',
+  'Elephant',
+  'Fish',
+  'Goldfish',
+  'Hot Pepper',
+  'Iguana',
+  'Jellyfish',
+  'Killer Kangaroos',
+  'Lionfish',
+  'Monkey',
+  'Neon',
+  'Octopus',
+  'Potato',
+  'Quill',
+  'Read',
+  'Snake',
+  'Trouble',
+  'Umbrella',
+  'Vulture',
+  'Water',
+  'Xerox',
+  'Yelling',
+  'Zero',
+]
+var randomNumber = Math.floor(Math.random() * (test.length));
+
+function newPrompt() {
+
+}
