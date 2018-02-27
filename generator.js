a542b239bc6e6ecba22f5290fae81b85c22dfc6f
Osiris <34219959+OsirisRex@users.noreply.github.com>
2018-02-26 19:40:39 -0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,11 +1,23 @@
 var person = [
   'Murderer',
   'Victim',
+  'Someone lost',
+  'A girl who just lost her mother',
+  'A boy who just lost his father',
 ]
-
+var images = [
+  'Images/barn1.jpg',
+  'Images/street1.jpg',
+  'Images/forest1.jpg',
+]
 function newPrompt() {
   var randomNumber1 = Math.floor(Math.random() * (person.length));
   var randomNumber2 = Math.floor(Math.random() * (person.length));
+  var imageNumber = Math.floor(Math.random()*(images.length));
+  while (randomNumber1 == randomNumber2) {
+    randomNumber2 = Math.floor(Math.random() * (person.length));
+  }
   document.getElementById('Person1').innerHTML = person[randomNumber1];
   document.getElementById('Person2').innerHTML = person[randomNumber2];
+  document.getElementById('image').src = images[imageNumber];
 }
